373331a624744e93d6c5d60ce0fda76fa1ad8a22
Dayane <dayanebmartins@gmail.com>
2021-07-10 20:53:23 -0300

    adiciona ex 5

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 
 
-    
+
+//Incluir texto  digitado no imput//
+const botaoIncluirTarefa = document.getElementById("criar-tarefa");//botyão
+const listaTarefas = document.getElementById("lista-tarefas");//ol
+const inputTarefa = document.getElementById("texto-tarefa");//imput
+
+botaoIncluirTarefa.addEventListener('click', adicionaTarefa)
+
+function adicionaTarefa(event) {
+    const textoLista = inputTarefa.value;
+    const itensLista = document.createElement("li");
+    itensLista.innerText = textoLista;
+    itensLista.classList.add("texto-item-list");
+    listaTarefas.appendChild(itensLista);
+    inputTarefa.value='';
+
+}
+
+
+
