cc81be0f02c866ad3a836f8452a9cd3a23e2e6f1
Dayane <dayanebmartins@gmail.com>
2021-07-12 19:51:52 -0300

    adiciona questão 7 e 8

--- script.js
+++ script.js
@@ -9,14 +9,75 @@ const inputTarefa = document.getElementById("texto-tarefa");//imput
 botaoIncluirTarefa.addEventListener('click', adicionaTarefa)
 
 function adicionaTarefa(event) {
-    const textoLista = inputTarefa.value;
-    const itensLista = document.createElement("li");
-    itensLista.innerText = textoLista;
-    itensLista.classList.add("texto-item-list");
-    listaTarefas.appendChild(itensLista);
-    inputTarefa.value='';
-
+  const textoLista = inputTarefa.value;
+  const itensLista = document.createElement("li");
+  itensLista.innerText = textoLista;
+  itensLista.classList.add("texto-item-list");
+  listaTarefas.appendChild(itensLista);
+  inputTarefa.value = '';
 }
+
+
+//function listarClick(event){
+//    limpaLista();
+//  event.target.style.backgroundColor= "rgb(128,128,128)";
+//}
+//function limpaLista(){
+//  const lista= document.querySelectorAll(".texto-item-list");
+//  for (let index = 0; index < lista.length; index+=1) {
+//       if(lista[index].style.backgroundColor==="rgb(128,128,128)"){
+//          lista[index].style.backgroundColor="white";
+//      }
+
+//   }
+//}
+
+
+
+
+
+
+
+
+
+
+const lista = document.getElementById("lista-tarefas");
+function changeBack() {
+  lista.addEventListener("click", function (event) {
+    let itemSelecionado = document.querySelectorAll(".select");//
+
+    for (let index = 0; index < itemSelecionado.length; index += 1) {
+      itemSelecionado[index].classList.remove("select");
+
+    }
+    event.target.classList.add("select");
+  })
+}
+
+
+changeBack();
+
+
+
+
+
+
+
+
+
+
+
+
+//let itensParaEscolha=document.querySelectorAll(".texto-item-list");
+//for (let index = 0; index < itensParaEscolha.length; index+=1){
+  //  itensParaEscolha[index].addEventListener("click", function(event){
+     //  let listenClick=document.getElementsByClassName("texto-item-list")
+     //  listenClick.style.backgrounColor= "rgb(128,128,128)";
+
+  //  }) // rebece uma função q tem um evento
+
+
+
 
 
 
